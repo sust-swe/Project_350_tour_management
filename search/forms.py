51077acd68b5f0148b47882ccab32cb8a32b0336
Shoaib tasrif <shoaib01@student.sust.edu>
2019-09-22 22:19:45 +0600

    restaurant search done

--- search/forms.py
+++ search/forms.py
@@ -8,9 +8,15 @@ class SearchRestaurantForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['country'] = forms.ChoiceField(
-            choices=[(o.id, str(o)) for o in Country.objects.all()])
+            choices=[(o.id, str(o)) for o in Country.objects.all()], required=False)
         self.fields['city'] = forms.ChoiceField(
-            choices=[(o.id, str(o)) for o in City.objects.none()])
+            choices=[(o.id, str(o)) for o in City.objects.none()], required=False)
 
         if 'country' in self.data:
-            country_id = int(self.data.get('country'))
+            try:
+                country_id = int(self.data.get('country'))
+                self.fields['city'] = forms.ChoiceField(
+                    choices=[(o.id, str(o)) for o in City.objects.filter(country_id=country_id)], required=False)
+
+            except (ValueError, TypeError):
+                pass
--- search/views.py
+++ search/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django import views
 from .forms import SearchRestaurantForm
 from homepage.models import City
@@ -17,20 +17,34 @@ class SearchRestaurant(views.View):
 
     def post(self, request):
         form = self.form_class(request.POST or None)
-        country_id = int(form.fields['country'].value())
-        city_id = int(form.fields['country'].value())
 
-        restaurants = Restaurant.objects.filter(
-            country_id=country_id, city_id=country_id)
-        context = {
-            'restaurants': restaurants,
-            'form': form,
-        }
-        return render(request, self.template_name, context)
+        if form.is_valid():
+            country_id = form.cleaned_data.get('country', None)
+            city_id = form.cleaned_data.get('city', None)
+            restaurant_name = form.cleaned_data.get('name', None)
+            # print(country_id, city_id, restaurant_name, type(country_id))
+            # print(Restaurant.objects.all())
+
+            if country_id:
+                restaurants = Restaurant.objects.filter(
+                    country_id=country_id)
+            # print(restaurants)
+            if city_id:
+                restaurants = restaurants.filter(city_id=int(city_id))
+            # print(restaurants)
+            if restaurant_name:
+                restaurants = restaurants.filter(
+                    name__contains=restaurant_name)
+            # print(restaurants)
+
+            return render(request, self.template_name, {'restaurants': restaurants, 'form': form})
+        else:
+            # print(form.as_table())
+            return render(request, self.template_name, {'form': form})
 
 
 def load_city(request):
     country_id = int(request.GET.get('country'))
     cities = City.objects.filter(country_id=country_id)
-    print(cities)
+    # print(cities)
     return render(request, 'ajax_city.html', {'cities': cities})
